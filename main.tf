c074b3f4d16c8c60704754005bf982a215c7cac4
fireboy1919 <rusty.phillips@gmail.com>
2018-07-17 16:48:18 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "dynamodb_label" {
-  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.1.2"
+  source     = "git::https://github.com/fireboy1919/terraform-terraform-label.git?ref=tags/0.1.2"
   namespace  = "${var.namespace}"
   stage      = "${var.stage}"
   name       = "${var.name}"
